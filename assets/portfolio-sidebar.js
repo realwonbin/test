16020739a7c07c83472cb8a5d3b07226676d76da
이영 <leeyoung@YoungMac.local>
2026-09-08 13:16:29 +0900

    hopw;iehr

--- assets/portfolio-sidebar.js
+++ assets/portfolio-sidebar.js
@@ -62,7 +62,7 @@
 
   const textSection = `
     <section class="sidebar-texts">
-      <h2>생각들</h2>
+      <h2>생각</h2>
       <a href="${rootPrefix}texts/tangled-thread.html?v=9"><span>존재에 대한</span>엉킨 실타래, 달항아리</a>
       <a href="${rootPrefix}texts/this-text-does-not-explain.html?v=9"><span>매체에 대한</span>이 글은 작업을 설명하지 않습니다</a>
       <a href="${rootPrefix}texts/near-things.html?v=9"><span>윤리에 대한</span>가까운 것들은 가장 멀리 있다</a>
